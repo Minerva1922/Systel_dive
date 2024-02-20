6e9229124a0a83bb58f6d249525ff2c23f78258d
Minerva1922 <minervapv83@gmail.com>
2024-02-20 23:55:51 +0100

    componente navbar visible, rutas completas

--- Systel_dive/src/App.svelte
+++ Systel_dive/src/App.svelte
@@ -1,63 +1,18 @@
+<!-- App.svelte -->
 <script>
-  let isOpen = false;
-
-  function toggleNavbar() {
-    isOpen = !isOpen;
-  }
+  import Navbar from './components/Navbar.svelte';
 </script>
 
-<nav class:open={isOpen} class="navbar">
-  <button on:click={toggleNavbar} class="toggle-btn">
-    ☰
-  </button>
-  <ul>
-    <li><a href="/quienes-somos">Quienes somos</a></li>
-    <li><a href="/que-hacemos">Que hacemos</a></li>
-    <li><a href="/camaras">Cámaras</a></li>
-    <li><a href="/contacto">Contacto</a></li>
-  </ul>
-</nav>
+<main>
+  <h1>Mi aplicación Svelte</h1>
+  <Navbar />
+  <!-- Aquí colocarías el resto de tu aplicación -->
+</main>
 
 <style>
-  .navbar {
-    width: 200px;
-    height: 100%;
-    background-color: #333;
-    position: fixed;
-    top: 0;
-    left: -200px;
-    transition: left 0.3s ease;
-  }
-
-  .open {
-    left: 0;
-  }
-
-  .toggle-btn {
-    background: none;
-    border: none;
-    color: rgb(3, 3, 3);
-    font-size: 1.5rem;
-    cursor: pointer;
-  }
-
-  ul {
-    list-style-type: none;
-    padding: 0;
-    margin: 0;
-  }
-
-  li {
-    padding: 10px;
-  }
-
-  a {
-    color: rgb(7, 7, 7);
-    text-decoration: none;
-  }
-
-  a:hover {
-    text-decoration: underline;
+  /* Estilos globales de tu aplicación */
+  main {
+    padding: 20px;
   }
 </style>
 
---
+++ Systel_dive/src/components/Navbar.svelte
@@ -0,0 +1,63 @@
+<!-- components/Navbar.svelte -->
+<script>
+  let isOpen = false;
+
+  function toggleNavbar() {
+    isOpen = !isOpen;
+  }
+</script>
+
+<nav class:open={isOpen} class="navbar">
+  <button on:click={toggleNavbar} class="toggle-btn">
+    ☰
+  </button>
+  <ul>
+    <li><a href="/quienes-somos">Quienes somos</a></li>
+    <li><a href="/que-hacemos">Que hacemos</a></li>
+    <li><a href="/camaras">Cámaras</a></li>
+    <li><a href="/contacto">Contacto</a></li>
+  </ul>
+</nav>
+
+<style>
+  .navbar {
+    width: 200px;
+    height: 100%;
+    background-color: #333;
+    position: fixed;
+    top: 0;
+    left: -200px;
+    transition: left 0.3s ease;
+  }
+
+  .open {
+    left: 0;
+  }
+
+  .toggle-btn {
+    background: none;
+    border: none;
+    color: rgb(3, 3, 3);
+    font-size: 1.5rem;
+    cursor: pointer;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  li {
+    padding: 10px;
+  }
+
+  a {
+    color: rgb(7, 7, 7);
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+</style>
