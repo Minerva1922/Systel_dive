e5a6b152fbebdfb08b3d00426a107fd05b9a0c62
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2024-03-04 23:11:13 +0100

    Background styles

--- Systel_dive/src/App.svelte
+++ Systel_dive/src/App.svelte
@@ -1,9 +1,13 @@
 <!-- App.svelte -->
 <script>
   import Navbar from './components/Navbar.svelte';
+
+  let fondodepantalla = 'fondo';
+  let src = '/img/fondoDePantalla.jpg'
 </script>
 
 <main>
+  <img {src} alt="{fondodepantalla} fondo" />
   <Navbar/>
   
   <!-- Aquí colocarías el resto de tu aplicación -->
@@ -13,6 +17,11 @@
   /* Estilos globales de tu aplicación */
   main {
     padding: 20px;
+    background-image: url(fondoDePantalla.jpg);
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-attachment: fixed;
   }
 </style>
 
